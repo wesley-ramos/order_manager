f00962e73901f57cafaa879b3030d7ff09dd6b6d
Wesley Ramos <wesley.ramos.developer@gmail.com>
2023-10-18 16:44:49 -0300

    Creating the initial project structure

---
+++ src/test/java/br/com/smartconsulting/ordermanager/OrdermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartconsulting.ordermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
