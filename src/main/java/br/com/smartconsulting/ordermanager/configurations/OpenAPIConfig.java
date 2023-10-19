ff20ef85d82dcd6d9031116e4e8d6c09c1f23b9b
Wesley Ramos <wesley.ramos.developer@gmail.com>
2023-10-19 00:46:29 -0300

    Configuring API documentation

---
+++ src/main/java/br/com/smartconsulting/ordermanager/configurations/OpenAPIConfig.java
@@ -0,0 +1,28 @@
+package br.com.smartconsulting.ordermanager.configurations;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenAPIConfig {
+	
+	@Bean
+	public OpenAPI api(ServletContext servletContext) {
+		Info info = new Info()
+			.title("Order Manager")
+			.description("API for order management")
+			.version("1.0.0")
+			.license(new License().name("Apache 2.0").url("http://springdoc.org"));
+        
+		return new OpenAPI()
+			.components(new Components())
+			.info(info);
+	}
+}
