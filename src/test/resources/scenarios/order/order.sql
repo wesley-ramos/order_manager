13fc868dc9d2afa1cc4562608b383f741a8a3be2
Wesley Ramos <wesley.ramos.developer@gmail.com>
2023-10-20 05:29:21 -0300

    Changed hibernate mapping

--- src/test/resources/scenarios/order/order.sql
+++ src/test/resources/scenarios/order/order.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (id, name, email) VALUES (1, 'Wesley Ramos', 'wesley.ramos@gmail.com');
 INSERT INTO products (id, name) VALUES (1, 'Coca cola 2L');
 
-INSERT INTO orders (id, product_id, user_id, quantity, status, created_at) VALUES (1, 1, 1, 5, 'COMPLETED', '2023-10-20 22:00:00');
-INSERT INTO orders (id, product_id, user_id, quantity, status, created_at) VALUES (2, 1, 1, 8, 'PENDING','2023-10-21 12:00:00');
+INSERT INTO orders (id, product_id, user_id, quantity, completed, created_at) VALUES (1, 1, 1, 5, false, '2023-10-20 22:00:00');
+INSERT INTO orders (id, product_id, user_id, quantity, completed, created_at) VALUES (2, 1, 1, 8, false,'2023-10-21 12:00:00');
