9c37dba0026459b691885372842954ae27844e56
Wesley Ramos <wesley.ramos.developer@gmail.com>
2023-10-22 01:14:06 -0300

    Creating notification of completed order

--- src/main/java/br/com/smartconsulting/ordermanager/configurations/AppConfig.java
+++ src/main/java/br/com/smartconsulting/ordermanager/configurations/AppConfig.java
@@ -1,5 +1,8 @@
 package br.com.smartconsulting.ordermanager.configurations;
 
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
@@ -14,5 +17,14 @@ public class AppConfig {
 		SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
 		eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
 		return eventMulticaster;
+	}
+	
+	@Bean
+	public VelocityEngine velocity() {
+		VelocityEngine velocityEngine = new VelocityEngine();
+		velocityEngine.setProperty("class.resource.loader.class", ClasspathResourceLoader.class.getName());
+		velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "class,file");
+        velocityEngine.init();
+        return velocityEngine;
 	}
 }
